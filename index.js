678aa9fbdeb6955d65eefff47cdcd10f12e2300a
kelsonbatista <kelsonbatista@gmail.com>
2022-05-06 14:38:41 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const app = require('./app');
-require('dotenv').config();
+const app = require("./app");
+require("dotenv").config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+// start
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
