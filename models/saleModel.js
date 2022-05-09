a14469b2894b233052d44b7fbcdf107f6cdc572a
kelsonbatista <kelsonbatista@gmail.com>
2022-05-09 17:51:29 -0300

    fix: return array in saleModel

--- models/saleModel.js
+++ models/saleModel.js
@@ -2,13 +2,13 @@ const connection = require('../config/connection');
 
 const getAllSales = async () => {
   const query = 'SELECT * FROM sales';
-  const result = await connection.execute(query);
+  const [result] = await connection.execute(query);
   return result;
 };
 
 const getSalesById = async (id) => {
   const query = 'SELECT * FROM sales WHERE sales.id = ?';
-  const result = await connection.execute(query, [id]);
+  const [result] = await connection.execute(query, [id]);
   if (!result.length) return null;
   return result;
 };
