ab0393a693ee3016f6c214f27e433236f6547c59
kelsonbatista <kelsonbatista@gmail.com>
2022-05-09 17:05:22 -0300

    feat: added router for sales

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const productRouter = require('./productRouter');
+const saleRouter = require('./saleRouter');
 
 const router = express.Router();
 
 router.use('./products', productRouter);
+router.use('./sales', saleRouter);
 
 module.exports = router;
---
+++ routes/saleRouter/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const saleController = require('../../controllers/saleController');
+
+const saleRouter = express.Router();
+
+saleRouter.get('./', saleController.getAllSales);
+
+saleRouter.get('/:id', saleController.getSalesById);
+
+module.exports = saleRouter;
