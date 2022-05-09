b5689c6ffb1d9c97df342eea0617f926ecd375ae
kelsonbatista <kelsonbatista@gmail.com>
2022-05-09 15:53:52 -0300

    feat: added getAllProducts/ById functions in productController

---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+const { StatusCodes } = require('http-status-codes');
+const productService = require('../services/productService');
+
+const getAllProducts = async (_req, res, next) => {
+  try {
+    const result = await productService.getAllProducts();
+    return res.status(StatusCodes.OK).json(result);
+  } catch (err) {
+    console.log(`Error Status ${err.status} - ${err.message}`);
+    next(err);
+  }
+};
+
+const getProductsById = async (req, res, next) => {
+  try {
+    const result = await productService.getProductsById(req.params.id);
+    return res.status(StatusCodes.OK).json(result);
+  } catch (err) {
+    console.log(`Error Status ${err.status} = ${err.message}`);
+    next(err);
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductsById,
+};
