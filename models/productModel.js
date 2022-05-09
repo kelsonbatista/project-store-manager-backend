8644c27444df7908abf4fd70bfe87513308053e5
kelsonbatista <kelsonbatista@gmail.com>
2022-05-09 17:37:30 -0300

    fix: return array

--- models/productModel.js
+++ models/productModel.js
@@ -2,13 +2,13 @@ const connection = require('../config/connection');
 
 const getAllProducts = async () => {
   const query = 'SELECT * FROM products';
-  const result = await connection.execute(query);
+  const [result] = await connection.execute(query);
   return result;
 };
 
 const getProductsById = async (id) => {
   const query = 'SELECT * FROM products WHERE products.id = ?';
-  const result = await connection.execute(query, [id]);
+  const [result] = await connection.execute(query, [id]);
   if (!result.length) return null;
   return result;
 };
