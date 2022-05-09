e3b8c9ff5543fd6ed65e5670b0238c2b30f2c608
kelsonbatista <kelsonbatista@gmail.com>
2022-05-09 10:45:07 -0300

    feat: included more tests in getAll productsModels

--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -30,15 +30,40 @@ describe('Search for all products in the database', () => {
   });
 
   describe('if product exists', () => {
+
+    const resultExecute = [{
+      id: 1,
+      name: 'Product1',
+      quantity: 10
+    }]
+
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([resultExecute]);
+    })
+
+    after(() => {
+      connection.execute.restore();
+    })
+
     it('must return an array', async () => {
       const result = await productModel.getAll();
-      expect(result).to.be.an('array');
+      expect(result[0]).to.be.an('array');
     });
 
     it('the array must be not empty', async () => {
       const result = await productModel.getAll();
       expect(result).to.be.not.empty;
     });
+
+    it('the array contain objects', () => {
+      const result = await productModel.getAll();
+      expect(result).to.be.an('object');
+    });
+
+    if('objects contain attributes id, name, quantity', () => {
+      const result = await productModel.getAll();
+      expect(result).to.be.includes.all.keys('id', 'name', 'quantity');
+    })
   });
 });
 
