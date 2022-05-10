c33e58b56d25c447bdfc0227b49bdcf03974660b
kelsonbatista <kelsonbatista@gmail.com>
2022-05-10 16:52:24 -0300

    feat: added route to create products

--- routes/productRouter/index.js
+++ routes/productRouter/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const productController = require('../../controllers/productController');
+const validateProduct = require('../../middlewares/validateProductMiddleware');
 
 const productRouter = express.Router();
 
 productRouter.get('/', productController.getAllProducts);
 
 productRouter.get('/:id', productController.getProductsById);
+
+productRouter.post('/', validateProduct, productController.createProduct);
 
 module.exports = productRouter;
