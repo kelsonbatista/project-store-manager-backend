6065166ed23e7f9644ca335e10039cb02aa50d00
kelsonbatista <kelsonbatista@gmail.com>
2022-05-11 17:32:33 -0300

    refactor: map sales for validation in sale middleware

--- middlewares/validateSaleMiddleware.js
+++ middlewares/validateSaleMiddleware.js
@@ -21,17 +21,20 @@ const validateSale = (req, _res, next) => {
 };
 
 const validateSaleProduct = (req, _res, next) => {
-  const [{ productId, quantity }] = req.body;
-  const { error } = productSchema.validate({ productId, quantity });
-  let type = '';
-  if (error) type = error.details[0].type;
-  let status = '';
-  const types = ['string.min', 'string.max', 'string.base',
-    'number.min', 'number.max', 'number.base'];
-  const resultType = types.some((item) => item === type);
-  if (resultType) status = StatusCodes.UNPROCESSABLE_ENTITY;
-  if (type === 'any.required') status = StatusCodes.BAD_REQUEST;
-  if (error) next({ status, message: error.message });
+  // const [{ productId, quantity }] = req.body;
+  (req.body).map(({ productId, quantity }) => {
+    const { error } = productSchema.validate({ productId, quantity });
+    let type = '';
+    if (error) type = error.details[0].type;
+    let status = '';
+    const types = ['string.min', 'string.max', 'string.base',
+      'number.min', 'number.max', 'number.base'];
+    const resultType = types.some((item) => item === type);
+    if (resultType) status = StatusCodes.UNPROCESSABLE_ENTITY;
+    if (type === 'any.required') status = StatusCodes.BAD_REQUEST;
+    if (error) return next({ status, message: error.message });
+    return true;
+  });
   next();
 };
 
