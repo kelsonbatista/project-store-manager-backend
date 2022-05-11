0ff1c68a3ceb82e4660765ddd269a37bd755d70b
kelsonbatista <kelsonbatista@gmail.com>
2022-05-11 10:11:39 -0300

    feat: added checkProductId in saleService

--- services/saleService.js
+++ services/saleService.js
@@ -1,4 +1,6 @@
+const { StatusCodes } = require('http-status-codes');
 const saleModel = require('../models/saleModel');
+const connection = require('../config/connection');
 
 const getAllSales = async () => {
   const result = await saleModel.getAllSales();
@@ -15,7 +17,18 @@ const createSaleProduct = async (productId, quantity) => {
   return result;
 };
 
+const checkSaleProductId = async (id) => {
+  const query = 'SELECT sale_id FROM sales_products WHERE sale_id = ?';
+  const [result] = await connection.execute(query, [id]);
+  return result;
+};
+
 const updateSaleProduct = async (saleId, productId, quantity) => {
+  const check = await checkSaleProductId(saleId);
+  if (!check.length) {
+    const error = { status: StatusCodes.NOT_FOUND, message: 'Sale not found' };
+    throw error;
+  }
   const result = await saleModel.updateSaleProduct(saleId, productId, quantity);
   return result;
 };
