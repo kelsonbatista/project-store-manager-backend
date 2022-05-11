5b17dd1afc75a5149b383c44af51ea2ffeb6dfc9
kelsonbatista <kelsonbatista@gmail.com>
2022-05-10 23:26:34 -0300

    feat: added create sale route

--- routes/saleRouter/index.js
+++ routes/saleRouter/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const saleController = require('../../controllers/saleController');
+const validateSale = require('../../middlewares/validateSaleMiddleware');
 
 const saleRouter = express.Router();
 
 saleRouter.get('/', saleController.getAllSales);
 
 saleRouter.get('/:id', saleController.getSalesById);
+
+saleRouter.post('/', validateSale, saleController.createSaleProduct);
 
 module.exports = saleRouter;
