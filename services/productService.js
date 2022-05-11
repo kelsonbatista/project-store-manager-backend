6f135925126b5da83e4a2a66bc5ef1f08b786352
kelsonbatista <kelsonbatista@gmail.com>
2022-05-11 09:25:41 -0300

    feat: added check product name validation in productService

--- services/productService.js
+++ services/productService.js
@@ -1,4 +1,6 @@
+const { StatusCodes } = require('http-status-codes');
 const productModel = require('../models/productModel');
+const connection = require('../config/connection');
 
 const getAllProducts = async () => {
   const result = await productModel.getAllProducts();
@@ -10,7 +12,18 @@ const getProductsById = async (id) => {
   return result;
 };
 
+const checkProductName = async (name) => {
+  const query = 'SELECT name FROM products WHERE name = ?';
+  const [result] = await connection.execute(query, [name]);
+  return result;
+};
+
 const createProduct = async (name, quantity) => {
+  const check = await checkProductName(name);
+  if (check.length) {
+    const error = { status: StatusCodes.CONFLICT, message: 'Product already exists' };
+    throw error;
+  }
   const result = await productModel.createProduct(name, quantity);
   return result;
 };
