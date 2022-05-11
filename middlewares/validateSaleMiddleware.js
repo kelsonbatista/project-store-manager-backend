f58f18f3035cc384261daf48bde182c36665c2bc
kelsonbatista <kelsonbatista@gmail.com>
2022-05-10 23:27:10 -0300

    feat: added validateSaleMiddleware

---
+++ middlewares/validateSaleMiddleware.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+const { StatusCodes } = require('http-status-codes');
+
+const validateSale = (req, _res, next) => {
+  const productSchema = Joi.object({
+    productId: Joi.number().integer().required(),
+    quantity: Joi.number().integer().required().min(1),
+  });
+
+  const { productId, quantity } = req.body;
+  const { error } = productSchema.validate({ productId, quantity });
+  let type = '';
+  if (error) type = error.details[0].type;
+  let status = '';
+  const types = ['string.min', 'string.max', 'string.base',
+    'number.min', 'number.max', 'number.base'];
+  const resultType = types.some((item) => item === type);
+  if (resultType) status = StatusCodes.UNPROCESSABLE_ENTITY;
+  if (type === 'any.required') status = StatusCodes.BAD_REQUEST;
+  if (error) next({ status, message: error.message });
+  next();
+};
+
+module.exports = validateSale;
