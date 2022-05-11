131a6c53d0154dcd13cc32cdc885a6597d1c4727
kelsonbatista <kelsonbatista@gmail.com>
2022-05-11 13:11:04 -0300

    fix: sale id validation / new product id validation in saleService

--- services/saleService.js
+++ services/saleService.js
@@ -17,16 +17,27 @@ const createSaleProduct = async (productId, quantity) => {
   return result;
 };
 
-const checkSaleProductId = async (id) => {
+const checkSaleId = async (id) => {
   const query = 'SELECT sale_id FROM sales_products WHERE sale_id = ?';
   const [result] = await connection.execute(query, [id]);
   return result;
 };
 
+const checkProductId = async (productId) => {
+  const query = 'SELECT id FROM products WHERE id = ?';
+  const [result] = await connection.execute(query, [productId]);
+  return result;
+};
+
 const updateSaleProduct = async (id, productId, quantity) => {
-  const check = await checkSaleProductId(id);
-  if (!check.length) {
+  const checkSale = await checkSaleId(id);
+  if (!checkSale.length) {
     const error = { status: StatusCodes.NOT_FOUND, message: 'Sale not found' };
+    throw error;
+  }
+  const checkProduct = await checkProductId(productId);
+  if (!checkProduct.length) {
+    const error = { status: StatusCodes.NOT_FOUND, message: 'Product does not exist' };
     throw error;
   }
   const result = await saleModel.updateSaleProduct(id, productId, quantity);
@@ -34,7 +45,7 @@ const updateSaleProduct = async (id, productId, quantity) => {
 };
 
 const deleteSaleProduct = async (id) => {
-  const check = await checkSaleProductId(id);
+  const check = await checkSaleId(id);
   if (!check.length) {
     const error = { status: StatusCodes.NOT_FOUND, message: 'Sale not found' };
     throw error;
