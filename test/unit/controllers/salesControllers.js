4ad3af88b5127c6ae19fc698f4da5ff2125e0b0e
kelsonbatista <kelsonbatista@gmail.com>
2022-05-12 21:05:00 -0300

    feat: added sales by id tests in CS layers

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -7,9 +7,9 @@ const saleController = require('../../../controllers/saleController');
 describe('2.1) SALES CONTROLLER LAYER - Search for all sales in the database', () => {
   describe('if sale do not exists', () => {
 
-    const resultExecute = [[]];
-    const request = [];
-    const response = [];
+    const resultExecute = [];
+    const request = {};
+    const response = {};
 
     // fazer stub da resposta que eu espero
     // sinon é responsável por criar um duble, simulação de dados
@@ -37,5 +37,142 @@ describe('2.1) SALES CONTROLLER LAYER - Search for all sales in the database', (
       // testo se a resposta json corresponde a um array simulado pelo sinon
       expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
     });
+  });
+
+  describe('if sale do exists', () => {
+
+    const resultExecute = [
+      {
+        "date": "2021-09-09T04:54:29.000Z",
+        "productId": 1,
+        "quantity": 2
+      }
+    ];
+
+    const request = {};
+    const response = {};
+
+    // fazer stub da resposta que eu espero
+    // sinon é responsável por criar um duble, simulação de dados
+    beforeEach(() => {
+      //simula o retorno do status e json
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(saleService, 'getAllSales').resolves(resultExecute);
+    });
+
+    afterEach(() => {
+      saleService.getAllSales.restore();
+    })
+
+    // é testado primeiramente em productController para simular uma req/res
+    it('must return status code 200', async () => {
+      // nao é salvo em variavel pois eu não quero o retorno, quero a propria execucao, a resposta direta
+      await saleController.getAllSales(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('must return json with an array', async () => {
+      await saleController.getAllSales(request, response);
+      // testo se a resposta json corresponde a um array simulado pelo sinon
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });
+
+    // it('must return objects in the array', async () => {
+    //   await saleController.getAllSales(request, response);
+    //   expect(response.json).to.includes.an('object');
+    // });
+  });
+});
+
+describe('2.2) SALES CONTROLLER LAYER - Search for sales by id', () => {
+  describe('if sale do not exists', () => {
+
+    const resultExecute = [
+      {
+        "date": "2021-09-09T04:54:29.000Z",
+        "productId": 1,
+        "quantity": 2
+      }
+    ];
+
+    const request = {};
+    const response = {};
+
+    // fazer stub da resposta que eu espero
+    // sinon é responsável por criar um duble, simulação de dados
+    beforeEach(() => {
+      //simula o retorno do status e json
+      request.params = { id: 22 }
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(saleService, 'getSalesById').resolves(resultExecute);
+    });
+
+    afterEach(() => {
+      saleService.getSalesById.restore();
+    })
+
+    // é testado primeiramente em productController para simular uma req/res
+    it('must return status code 200', async () => {
+      // nao é salvo em variavel pois eu não quero o retorno, quero a propria execucao, a resposta direta
+      await saleController.getSalesById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('must return json with an array', async () => {
+      await saleController.getSalesById(request, response);
+      // testo se a resposta json corresponde a um array simulado pelo sinon
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });
+  });
+
+  describe('if sale do exists', () => {
+
+    const resultExecute = [
+      {
+        "date": "2021-09-09T04:54:29.000Z",
+        "productId": 1,
+        "quantity": 2
+      }
+    ];
+    
+    const request = {};
+    const response = {};
+
+    // fazer stub da resposta que eu espero
+    // sinon é responsável por criar um duble, simulação de dados
+    beforeEach(() => {
+      //simula o retorno do status e json
+      request.params = { id: 1 }
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(saleService, 'getSalesById').resolves(resultExecute);
+    });
+
+    afterEach(() => {
+      saleService.getSalesById.restore();
+    })
+
+    // é testado primeiramente em productController para simular uma req/res
+    it('must return status code 200', async () => {
+      // nao é salvo em variavel pois eu não quero o retorno, quero a propria execucao, a resposta direta
+      await saleController.getSalesById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('must return json with an array', async () => {
+      await saleController.getSalesById(request, response);
+      // testo se a resposta json corresponde a um array simulado pelo sinon
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });
+
+    // it('must return objects in the array', async () => {
+    //   await saleController.getAllSales(request, response);
+    //   expect(response.json).to.includes.an('object');
+    // });
   });
 });
--- test/unit/services/salesServices.js
+++ test/unit/services/salesServices.js
@@ -6,11 +6,11 @@ const saleService = require('../../../services/saleService');
 describe('6.1) SALES SERVICES LAYER - Search for all sales in the database', () => {
   describe('if sales do not exists', () => {
 
-    const resultExecute = [[]];
+    const resultExecute = [];
 
     // sinon é responsável por criar um duble, simulação de dados
     beforeEach(() => {
-      sinon.stub(saleModel, 'getAllSales').resolves([]);
+      sinon.stub(saleModel, 'getAllSales').resolves(resultExecute);
     })
 
     afterEach(() => {
@@ -63,6 +63,79 @@ describe('6.1) SALES SERVICES LAYER - Search for all sales in the database', () 
 
     it('objects contain attributes saleId, productId, quantity, date', async () => {
       const result = await saleService.getAllSales();
+      expect(result[0]).to.have.all.keys('saleId', 'productId', 'quantity', 'date');
+    });
+  });
+});
+
+describe('6.2) SALES SERVICES LAYER - Search for sales by id', () => {
+  describe('if sales do not exists', () => {
+
+    const resultExecute = [];
+    const response = {};
+
+    // sinon é responsável por criar um duble, simulação de dados
+    beforeEach(() => {
+      sinon.stub(saleModel, 'getSalesById').resolves(resultExecute);
+      // response.json = sinon.stub().returns();
+    })
+
+    afterEach(() => {
+      saleModel.getSalesById.restore();
+    })
+
+    // it('must return an array', async () => {
+    //   const result = await saleService.getSalesById(11);
+    //   console.log(result, '<<<<<<<<<< result');
+    //   expect(result.calledWith(sinon.match.object)).to.be.equal('Sale not found');
+    // });
+
+    // it('must return an array', async () => {
+    //   const result = await saleService.getSalesById(11);
+    //   expect(result).to.be.an('array');
+    // });
+
+    // it('the array must be empty', async () => {
+    //   const result = await saleService.getSalesById(11);
+    //   expect(result).to.be.empty;
+    // });
+  });
+
+  describe('if sales exists', () => {
+
+    const resultExecute = [{
+      saleId: 1,
+      productId: 2,
+      quantity: 10,
+      date: '2022/05/10'
+    }];
+
+    // agora sao dados de saleModel, o Service não faz conexao com o banco
+    beforeEach(() => {
+      sinon.stub(saleModel, 'getSalesById').resolves(resultExecute);
+    })
+
+    afterEach(() => {
+      saleModel.getSalesById.restore();
+    })
+
+    it('must return an array', async () => {
+      const result = await saleService.getSalesById(1);
+      expect(result).to.be.an('array');
+    });
+
+    it('the array must be not empty', async () => {
+      const result = await saleService.getSalesById(1);
+      expect(result).to.be.not.empty;
+    });
+
+    it('the array contain objects', async () => {
+      const result = await saleService.getSalesById(1);
+      expect(result[0]).to.be.an('object');
+    });
+
+    it('objects contain attributes saleId, productId, quantity, date', async () => {
+      const result = await saleService.getSalesById(1);
       expect(result[0]).to.have.all.keys('saleId', 'productId', 'quantity', 'date');
     });
   });
